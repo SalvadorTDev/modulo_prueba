5854ec72d6c2ea640d8141edf6cf9d44471ac758
SalvadorTDev <juanstdev@gmail.com>
2023-11-23 17:09:21 -0500

    primer commit

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Modulo de reportes odoo v13',
+    'summary': 'Este módulo contiene nuevos reportes personalizados para la parte de ventas.',
+    'version': '13.0',
+    'author': 'Juan Salvador Toni',
+    # 'license': '',
+    'category': 'Uncategorized',
+
+    'depends': [
+        'base'
+    ],
+    'external_dependencies': {
+        'python': [
+
+        ]
+    },
+    'data': [
+        
+    ],
+    'installable': True
+
+}
